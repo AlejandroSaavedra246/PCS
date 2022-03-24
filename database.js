95fd170b7fdee67bcec1b0db9bc7ff48d502624d
AlejandroSaavedra246 <jaime.saavedra.is@unipolidgo.edu.mx>
2022-03-24 15:09:32 -0600

    Update change

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bc8364ebb9ef3c",
+	password: "3cc7c094",
 	port: 3306,
-	database: "adoption",
+	database: "heroku_755eb81efd60190",
 	secret_key: "qwertyuiopasdfghjklñzxcvbnM4"
 };
 exports.config = config;
